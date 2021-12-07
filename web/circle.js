cc4236b149f1b375d49bab856bf998219920f3d6
Jim Blackler <jimblackler@google.com>
2021-12-07 16:14:09 +0000

    Draw the circle.

--- web/circle.js
+++ web/circle.js
@@ -1,11 +1,22 @@
-export function drawCircle(imageData, width, height, cx, cy, radius) {
-  for (let idx = 0; idx !== 100; idx++) {
-    const x = Math.floor(Math.random() * width);
-    const y = Math.floor(Math.random() * height);
-    const offset = (y * imageData.width + x) * 4;
-    imageData.data[offset] = 0;
-    imageData.data[offset + 1] = 0;
-    imageData.data[offset + 2] = 0;
-    imageData.data[offset + 3] = 255;
+function plot(imageData, x, y, r, g, b, a) {
+  const offset = (Math.floor(y) * imageData.width + Math.floor(x)) * 4;
+  imageData.data[offset] = r;
+  imageData.data[offset + 1] = g;
+  imageData.data[offset + 2] = b;
+  imageData.data[offset + 3] = a;
+}
+
+export function drawCircle(imageData, cx, cy, radius) {
+  for (let i = radius / Math.sqrt(2); i >= 0; i--) {
+    const j = Math.sqrt(radius * radius - i * i);
+    plot(imageData, cx - i, cy + j, 0, 0, 0, 255);
+    plot(imageData, cx + i, cy + j, 0, 0, 0, 255);
+    plot(imageData, cx - i, cy - j, 0, 0, 0, 255);
+    plot(imageData, cx + i, cy - j, 0, 0, 0, 255);
+    plot(imageData, cx - j, cy + i, 0, 0, 0, 255);
+    plot(imageData, cx + j, cy + i, 0, 0, 0, 255);
+    plot(imageData, cx - j, cy - i, 0, 0, 0, 255);
+    plot(imageData, cx + j, cy - i, 0, 0, 0, 255);
   }
+
 }
--- web/script.js
+++ web/script.js
@@ -5,6 +5,6 @@ const ctx = canvas.getContext('2d');
 canvas.width = 400;
 canvas.height = 400;
 const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-drawCircle(imageData, canvas.width, canvas.height, canvas.width / 2, canvas.height / 2, 80);
+drawCircle(imageData, canvas.width / 2, canvas.height / 2, 180);
 ctx.putImageData(imageData, 0, 0);
 document.body.appendChild(canvas);
